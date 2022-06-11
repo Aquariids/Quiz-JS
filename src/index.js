9e70bde9fe6932c0b7acc6afcf1437f4d06c7dc9
Dima-kun <FaboritePonyville@gmail>
2022-06-11 21:18:32 +0300

    add commentary

--- src/index.js
+++ src/index.js
@@ -5,28 +5,37 @@ import * as additionalFun from "./module/additionalFun";
 import allDomElements from "./module/domElements";
 import * as bodyQuiz from './module/bodyQuiz';
 import button from './module/button';
+const {
+    loadQuiz,
+    getSelected
+} = bodyQuiz;
+const {
+    deleteSelectAnswers,
+    totalQuest,
+    hideCode,
+    showCodeQuiz,
+} = additionalFun;
+const {
+    total,
+    codeForQuestion
+} = allDomElements
 
 document.addEventListener('DOMContentLoaded', () => {
-    const {loadQuiz,getSelected} = bodyQuiz;
-    const {
-        deleteSelectAnswers,
-        totalQuest,
-        hideCode,
-        showCodeQuiz,
-    } = additionalFun;
-    const {
-        total,
-        codeForQuestion
-    } = allDomElements
     // Текущий вопросо
     let currentQuiz = 0;
     // количество правильно отвеченых вопросов
     let score = 0;
-    loadQuiz(currentQuiz,deleteSelectAnswers,allDomElements,quizData);
+    // подгружаем ответы и вопросы
+    loadQuiz(currentQuiz, deleteSelectAnswers, allDomElements, quizData);
+    // скрываем код для вопросов в html
     hideCode(codeForQuestion);
+
+    // показываем нужный код для конкретного вопроса
     showCodeQuiz(codeForQuestion, currentQuiz);
+
     // при нажатии на кнопку  подгружаем след вопрос, если был дан ответ
-    button(currentQuiz,score,additionalFun,allDomElements,getSelected,quizData,loadQuiz);
+    button(currentQuiz, score, additionalFun, allDomElements, getSelected, quizData, loadQuiz);
+    // показываем количество вопросов и на каком находимся.
     totalQuest(total, currentQuiz, quizData.length);
-   
+
 });
